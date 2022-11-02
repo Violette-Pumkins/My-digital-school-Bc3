2d4a7384ef296f4d986fec7bacbbe0d916689e63
Violette Pumkins <violette.pumkins@gmail.com>
2022-11-02 10:53:47 +0100

    poo

--- POO/solid1/src/Reporting/Format/HtmlSpecialFormatter.php
+++ POO/solid1/src/Reporting/Format/HtmlSpecialFormatter.php
@@ -4,7 +4,8 @@ namespace App\Reporting\Format;
 use App\Reporting\Report;
 use App\Reporting\Format\HtmlFormatter;
 
-class HtmlSpecialFormatter extends HtmlFormatter{
+class HtmlSpecialFormatter extends HtmlFormatter
+{
     public function formatToHTML(Report $report): string
     {
         $html = parent::formatToHTML($report);
