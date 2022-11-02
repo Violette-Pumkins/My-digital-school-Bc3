03c3a63c670de6b8018a8ec52c270cbe3e37b268
Violette Pumkins <violette.pumkins@gmail.com>
2022-11-02 16:52:48 +0100

    deserialize

--- POO/solid1/src/Reporting/Format/HtmlFormatter.php
+++ POO/solid1/src/Reporting/Format/HtmlFormatter.php
@@ -5,28 +5,52 @@ use App\Reporting\Report;
     class HtmlFormatter{
 
 
-    /**
-     * Retourne le rapport formatté en HTML
-     *
-     * @return string
-     */
-    public function formatToHTML(Report $report): string
-    {
-        $contents = $report->getContents();
-        $data = "";
+        /**
+         * Retourne le rapport formatté en HTML
+         *
+         * @return string
+         */
+        public function formatToHTML(Report $report): string
+        {
+            $contents = $report->getContents();
+            $data = "";
 
-        foreach ($contents['data'] as $value) {
-            $data .= "<li>$value</li>";
+            foreach ($contents['data'] as $value) {
+                $data .= "<li>$value</li>";
+            }
+
+            return "
+                <h2> {$contents['title']} </h2>
+                <em>Date : {$contents['date']} </em>
+                <h4>Données : </h4>
+                <ul>
+                    $data
+                </ul>
+            ";
         }
-
-        return "
-            <h2> {$contents['title']} </h2>
-            <em>Date : {$contents['date']} </em>
-            <h4>Données : </h4>
-            <ul>
-                $data
-            </ul>
-        ";
-    }
+        /**
+         * interface CoffeeMaker{
+         * public function makeCoffee();
+         * public function makeCapuccino();
+         * public function makeTea();
+         * public function makeTotamtoSoup();
+         * 
+         * }
+         * class Machineacafe implement coffeeMaker{
+            * public function makeCoffee(){
+            * //lignes de code
+            * }
+            * public function makeCapuccino(){
+            * //lignes de code
+            * }
+            * public function makeTea(){
+            * //lignes de code
+            * }
+            * public function maketomatoSoup(){
+            * //lignes de code
+            * }
+         * }
+         * Ne repecte pas le SRP
+         */
 
     }
